a438cee42f3c59a80618bdb5e49243201f1a9a20
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-04-14 10:53:52 +0800

    update: init the project

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "@chakra-ui/react";
+
+function Main() {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default Main;
