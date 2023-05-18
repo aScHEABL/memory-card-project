601e166884ebb95b1c2d726e668a068607f3b08e
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-05-18 17:27:52 +0800

    WIP: import images dynamically

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,26 @@
 import React from "react";
-import { Container, Box } from "@chakra-ui/react";
+import { Container, Flex, Heading } from "@chakra-ui/react";
+
+const cache = {};
+
+function importAll(r) {
+    r.keys().forEach((key) => cache[key] = r(key));
+}
+
+importAll(require.context("../images", false, /\.(png|jpe?g|svg)$/));
+
+// const images = Object.entries(cache).map(module => module[1].default);
+const img_array = new Map(Object.entries(cache));
+const imgNodes = img_array.forEach((img) => (<img src={img.value}></img>))
 
 function Main() {
     return (
-        <Box>
-
-        </Box>
+        <Container as='main'>
+            <Flex wrap='wrap'>
+                { imgNodes }
+            </Flex>
+            {/* <Heading>{imgNodes}</Heading> */}
+        </Container>
     )
 }
 
