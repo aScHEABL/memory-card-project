e00874155953f73d3d13f777c126bdb367359b54
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-05-19 00:57:02 +0800

    update: uniqueID for image nodelist

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Flex, Heading } from "@chakra-ui/react";
+import { v4 as uuidv4 } from 'uuid';
 
 const cache = {};
 
@@ -10,7 +11,7 @@ function importAll(r) {
 importAll(require.context("../images", false, /\.(png|jpe?g|svg)$/));
 
 const images = Object.entries(cache).map((module) => module[1]);
-const imageNodes = images.map((item) => <img src={item}></img>)
+const imageNodes = images.map((item) => <img key={uuidv4()} src={item}></img>)
 
 
 
