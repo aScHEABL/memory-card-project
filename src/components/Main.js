3ce96c551e8c1876fa54209a8675adc2e33b5d2a
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-05-19 14:41:59 +0800

    update: randomly display 4 out of 66 imgs

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Container, Flex, Heading } from "@chakra-ui/react";
+import { Container, Flex, Icon, IconButton, Image, Box } from "@chakra-ui/react";
 import { v4 as uuidv4 } from 'uuid';
+import _ from 'lodash-es';
 
 const cache = {};
 
@@ -10,18 +11,32 @@ function importAll(r) {
 
 importAll(require.context("../images", false, /\.(png|jpe?g|svg)$/));
 
+const handleClick = (e) => {
+    console.log("Click handled!");
+    e.target.setAttribute("data-checked", true);
+}
+
 const images = Object.entries(cache).map((module) => module[1]);
-const imageNodes = images.map((item) => <img key={uuidv4()} src={item}></img>)
+// const imageNodes = images.map((item) => <Image width="25%" objectFit="scale-down" key={uuidv4()} src={item} style={{cursor: 'pointer'}} 
+// data-checked="false" onClick={(e) => handleClick(e)} />)
+const imageNodes = images.map((item) => 
+    <Box key={uuidv4()} boxSize="sm">
+        <Image objectFit="scale-down" src={item} style={{cursor: 'pointer'}} 
+        data-checked="false" onClick={(e) => handleClick(e)} />
+    </Box>
+)
+const randomImgNodes_sample = _.sampleSize(imageNodes, 4);
+
 
 
 
 function Main() {
+
     return (
-        <Container as='main'>
-            <Flex wrap='wrap'>
-                {imageNodes}
+        <Container as='main' maxW='180ch' centerContent>
+            <Flex wrap='nowrap' justify='center' alignItems='center'>
+                { randomImgNodes_sample }
             </Flex>
-            {/* <Heading>{imgNodes}</Heading> */}
         </Container>
     )
 }
