bf3b01d5e9d25ff2a376fccdbb9e964e9ecb7944
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-05-22 01:40:12 +0800

    WIP: image click trigger re-render

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,44 +1,69 @@
-import React from "react";
-import { Container, Flex, Icon, IconButton, Image, Box } from "@chakra-ui/react";
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState, useEffect } from 'react';
 import _ from 'lodash-es';
+import { Container, Flex, Image, Box } from '@chakra-ui/react';
+import { v4 as uuidv4 } from 'uuid';
 
-const cache = {};
+const Main = () => {
+  const [images, setImages] = useState([]);
+  const [renderedImages, setRenderedImages] = useState([]);
 
-function importAll(r) {
-    r.keys().forEach((key) => cache[key] = r(key));
-}
+  useEffect(() => {
+    // Import all the images from the image directory
+    const cache = {};
+    function importAll(r) {
+      r.keys().forEach((key) => (cache[key] = r(key)));
+    }
+    importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+    const images_src = Object.entries(cache).map((module) => module[1]);
 
-importAll(require.context("../images", false, /\.(png|jpe?g|svg)$/));
+    const imageNodes = images_src.map((item) => ({
+      id: uuidv4(),
+      src: item,
+      checked: false,
+    }));
 
-const handleClick = (e) => {
-    console.log("Click handled!");
-    e.target.setAttribute("data-checked", true);
-}
+    setImages(imageNodes);
+  }, []);
 
-const images = Object.entries(cache).map((module) => module[1]);
-// const imageNodes = images.map((item) => <Image width="25%" objectFit="scale-down" key={uuidv4()} src={item} style={{cursor: 'pointer'}} 
-// data-checked="false" onClick={(e) => handleClick(e)} />)
-const imageNodes = images.map((item) => 
-    <Box key={uuidv4()} boxSize="sm">
-        <Image objectFit="scale-down" src={item} style={{cursor: 'pointer'}} 
-        data-checked="false" onClick={(e) => handleClick(e)} />
-    </Box>
-)
-const randomImgNodes_sample = _.sampleSize(imageNodes, 4);
+  useEffect(() => {
+    renderImages();
+  }, [images]); // Rerender when images change
 
+  const renderImages = () => {
+    const randomImages = _.sampleSize(images, 4);
+    setRenderedImages(randomImages);
+  };
 
+  const handleImageClick = (id) => {
+    const updatedImages = images.map((image) =>
+      image.id === id ? { ...image, checked: true } : image
+    );
+    setImages(updatedImages);
+  };
 
+  useEffect(() => {
+    if (images.some((image) => image.checked)) {
+      renderImages();
+    }
+  }, [images]);
 
-function Main() {
-
-    return (
-        <Container as='main' maxW='180ch' centerContent>
-            <Flex wrap='nowrap' justify='center' alignItems='center'>
-                { randomImgNodes_sample }
-            </Flex>
-        </Container>
-    )
-}
+  return (
+    <Container as="main" maxW="180ch" centerContent>
+      <Flex wrap="nowrap" justify="center" alignItems="center">
+        {renderedImages.map((item) => (
+          <Box key={item.id} boxSize="sm">
+            <Image
+              objectFit="cover"
+              src={item.src}
+              style={{ cursor: 'pointer' }}
+              data-checked={item.checked}
+              onClick={() => handleImageClick(item.id)}
+            />
+          </Box>
+        ))}
+      </Flex>
+    </Container>
+  );
+};
 
 export default Main;
